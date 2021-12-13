0b4e915d416d23bee05429c4109352bf56c0e937
Galya <58034004+Galya777@users.noreply.github.com>
2021-12-13 20:58:40 +0200

    Add files via upload

---
+++ MorseCode.java
@@ -0,0 +1,57 @@
+package zad5;
+
+public class MorseCode {
+
+
+    String morseLetters(char letter){
+        return switch (letter) {
+            case 'A' -> ".-";
+            case 'B' -> "-...";
+            case 'C' -> "-.-.";
+            case 'D' -> "-..";
+            case 'E' -> ".";
+            case 'F' -> "..-.";
+            case 'G' -> "--.";
+            case 'H' -> "....";
+            case 'I' -> "..";
+            case 'J' -> ".---";
+            case 'K' -> "-.-";
+            case 'L' -> ".-..";
+            case 'M' -> "--";
+            case 'N' -> "-.";
+            case 'O' -> "---";
+            case 'P' -> ".--.";
+            case 'Q' -> "--.--";
+            case 'R' -> ".-.";
+            case 'S' -> "...";
+            case 'T' -> "-";
+            case 'U' -> "...-";
+            case 'V' -> "...-";
+            case 'W' -> ".--";
+            case 'X' -> "-..-";
+            case 'Y' -> "-.--";
+            case 'Z' -> "--..";
+            default -> "";
+        };
+    }
+    String encode(String plainText){
+        char[] text=plainText.toCharArray();
+        char[] encode= new char[plainText.length()*5];
+        int counter=0;
+        for(int i=0; i<plainText.length();++i){
+            String code= morseLetters(text[i]);
+            for(int j=0; j<code.length();++i){
+                encode[counter]=code.charAt(i);
+                ++counter;
+            }
+            encode[counter]=' ';
+            ++counter;
+        }
+     return new String(encode);
+    }
+    String decode(String morseCode){
+        char[] decode= new char[morseCode.length()/5];
+
+        return new String(decode);
+    }
+}
