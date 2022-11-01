75b3eb987df47acb572bd94feb48d23df3326ee1
Galya <58034004+Galya777@users.noreply.github.com>
2022-11-01 22:56:52 +0200

    Add files via upload

---
+++ Calculate2.java
@@ -0,0 +1,50 @@
+// Lab 2: Calculate2.java
+// Perform simple calculations on three integers.
+import javax.swing.JOptionPane;
+
+public class Calculate2 {
+
+   public static void main( String args[] )
+   {
+      String firstNumber;  // first string entered by user
+      String secondNumber; // second string entered by user
+      String thirdNumber;  // third string entered by user
+
+      int number1; // first number
+      int number2; // second number
+      int number3; // third number
+
+      int average;  // average of the numbers 
+      int largest;  // largest number
+      int product;  // product of the numbers
+      int smallest; // smallest number
+      int sum;      // sum of the numbers
+
+      /* write a series of statements to read in three numbers, convert them
+         to integers, and assign them to number1, number2, and number3 */
+
+      // initialize largest and smallest
+      largest = number1;
+      smallest = number2;
+
+      // determine smallest and largest
+      /* write code here that compares all three integers and sets the 
+         largest and smallest accordingly */
+
+      // perform calculations
+      sum = number1 + number2 + number3;
+      /* write statements to calculate the product and the average */
+
+      // create result string
+      String result;
+      /* Write a statement that concatenates all the results into a single string
+         "result" that is displayed on line from line 44 of the template code */
+
+      // Write a statement to output results
+
+   }
+
+} // end class Calculate2
+
+ 
+ 
---
+++ Shapes.java
@@ -0,0 +1,15 @@
+// Lab 1: Shapes.java
+// Program draws four shapes to the command window.
+
+public class Shapes {
+
+   public static void main( String args[] )
+   {
+      System.out.println();
+      /* write a series of statements that will print the shapes
+         to the command window */
+   }
+
+} // end class Shapes
+
+ 
