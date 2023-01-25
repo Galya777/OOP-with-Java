b16edcd31293a199e94518962fa56eedacb65117
Galya <58034004+Galya777@users.noreply.github.com>
2023-01-25 19:36:24 +0200

    Add files via upload

--- ArrayListTest.java
+++ ArrayListTest.java
@@ -1,93 +1,69 @@
-package task2;
+package task;
 
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.*;
+
+import static java.util.Collections.*;
 
 public class ArrayListTest {
+    ArrayList<String> text;
 
-    public static <E extends Comparable<E>> E max(E[][] list){
-       if(list==null||list.length==0||list[0].length==0){
-           return null;
-       }
-       E max =list[0][0];
-       for(int i=0; i<list.length;++i){
-           for(int j=0; j<list[0].length;++j){
-               if(max.compareTo(list[i][j])<0){
-                   max= list[i][j];
-               }
-           }
-       }
-       return max;
+    public void sort(){
+        text.sort(reverseOrder());
     }
 
-    public static <E> void shuffle(ArrayList<E> list){
-     if(list==null){
-         return;
-     }
-     Collections.shuffle(list);
-    }
+    public void sortByFrwequency() {
 
-    public static <E extends Comparable<E>> E max(ArrayList<E> list){
-        if (list == null) {
-            return null;
-        }
-        return Collections.max(list);
-    }
+        Map<Integer, Integer> elementCountMap = new LinkedHashMap<>();
 
-    public static <E> ArrayList<E> removeDuplicates(ArrayList<E> list){
-        if (list == null) {
-            return null;
-        }
-        ArrayList<E > noDuplicates= new ArrayList<>();
-        for(int i=0; i<list.size();++i){
-            if(Collections.frequency(list, list.get(i))==1){
-                noDuplicates.add(list.get(i));
+        //Check presence of each element in elementCountMap
+
+        for (String s : text) {
+            if (elementCountMap.containsKey(s)) {
+                //If element is present in elementCountMap, increment its value by 1
+
+                elementCountMap.put(Integer.valueOf(s), elementCountMap.get(s) + 1);
+            } else {
+                //If element is not present, insert this element with 1 as its value
+
+                elementCountMap.put(Integer.valueOf(s), 1);
             }
         }
-        return noDuplicates;
-    }
 
-    public static void main(String[] args) {
- Double[][] numbers= {
-         {3.4, 2.3, 7.1},
-         {-4.3, 4.9},
-         {1.2, 9.12, 9.1, 15.7}
- };
-System.out.println("Max: "+max(numbers));
+        //Construct an ArrayList holding all Entry objects of elementCountMap
 
-//b)
-        ArrayList<Integer> list= new ArrayList<>();
-        for(int i=0; i<21; ++i){
-            list.add(i);
+        ArrayList<Map.Entry<Integer, Integer>> listOfEntry = new ArrayList<>(elementCountMap.entrySet());
 
-        }
-        shuffle(list);
-        System.out.println("Shuffled list: "+list);
+        //Sort listOfEntry based on values
 
-       //c)
-        System.out.println("Max: "+max(list));
-        ArrayList<Car> cars= new ArrayList<>();
-        cars.add(new Car("model", 2000));
-        cars.add(new Car("model", 1998));
-        cars.add(new Car("model", 1999));
-        cars.add(new Car("model", 2004));
-        cars.add(new Car("model", 2015));
-        cars.add(new Car("model", 1989));
-     System.out.println("Max in cars: "+ max(cars).toString());
-
-
-
-        //d)
-        ArrayList<String> duplicates= new ArrayList<>();
-        duplicates.add("str1");
-        duplicates.add("str2");
-        duplicates.add("str1");
-        duplicates.add("str4");
-        duplicates.add("str5");
-        duplicates.add("str3");
-        System.out.println("Duplicates "+ removeDuplicates(list));
+        listOfEntry.sort(new Comparator<Map.Entry<Integer, Integer>>() {
+            @Override
+            public int compare(Map.Entry<Integer, Integer> o1, Map.Entry<Integer, Integer> o2) {
+                return o2.getValue().compareTo(o1.getValue());
+            }
+        });
 
     }
 
 
+    public static <T extends Comparable<? super T>>
+    T removeMax(List<T> list)
+    {
+    list.remove(list.stream().max((Comparator<? super T>) list));
+        return null;
+    }
+
+    public void getNames(){
+        text.stream().findAny();
+    }
+    public void searchNames(){
+     text.stream().findAny();
+    }
+    public void copyTo(ArrayList<String> str){
+        text=str;
+
+    }
+    public String toStringDescending(){
+        sort();
+        return text.toString();
+    }
 }
